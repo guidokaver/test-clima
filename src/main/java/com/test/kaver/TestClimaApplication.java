b0e232d212d572f139a27366d30e2940d020e434
guidokaver <guido.sanfilippo.kaver.challenge@gmail.com>
2024-10-04 12:03:22 -0300

    primer commit

---
+++ src/main/java/com/test/kaver/TestClimaApplication.java
@@ -0,0 +1,13 @@
+package com.test.kaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestClimaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestClimaApplication.class, args);
+    }
+
+}
